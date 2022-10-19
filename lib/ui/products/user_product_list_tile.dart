cf882e9782c10568948b167f45300afedf06eea6
DuongThiYenNhiB1909961 <nhib1808@gmail.com>
2022-10-19 21:28:10 +0700

    Quan ly trang thai voi provider - CartManager

---
+++ lib/ui/products/user_product_list_tile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../models/product.dart';
+
+import 'products_manager.dart';
+
+import 'package:provider/provider.dart';
+
+class UserProductListTile extends StatelessWidget{
+  final Product product;
+
+  const UserProductListTile(
+    this.product, {
+      super.key,
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(product.title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            buildEditButton(context),
+            buildDeteleButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDeteleButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.delete),
+      onPressed: () {
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+      },
+      color: Theme.of(context).errorColor,
+    );
+  }
+
+  Widget buildEditButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.edit),
+      onPressed: () {
+        print('Go to edit product screen');
+      },
+      color: Theme.of(context).primaryColor,
+    );
+  }
+}
